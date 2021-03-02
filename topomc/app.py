fixed point symbol scaling issue

topomc/app.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 
+import numpy as np
 import yaml
 
 from topomc import render
@@ -18,9 +19,11 @@ try:
         settings = yaml.full_load(stream)
 except Exception as e:
     Logger.log(logging.critical, f"Settings.yml is incorrectly formatted or missing")
-    raise e 
+    raise e
 
-def parse_args(args):
+
+def create_settings(args):
+
     try:
         bounding_points = x1, z1, x2, z2 = args.x1, args.z1, args.x2, args.z2
         settings["Bounding points"] = bounding_points
@@ -36,11 +39,10 @@ def parse_args(args):
 
     return settings
 
+
 def run(args):
 
-    curr_symbol = None
-
-    settings = parse_args(args)
+    settings = create_settings(args)
 
     Logger.log(logging.info, "Collecting chunks...")
     blockmap = BlockMap(settings["World"], *settings["Bounding points"])
@@ -55,9 +57,13 @@ def run(args):
     for process in processes:
         Logger.log(logging.info, f"Running {process.__class__.__name__} process...", sub=1)
         process.process()
-    
+
+    height = len(blockmap.heightmap[0])
+    width = len(blockmap.heightmap)
+    max_len = max(np.floor(width / 16), np.floor(height / 16))
+
     Logger.log(logging.info, "Creating symbols...")
-    symbols = [Symbol_SC(processes) for Symbol_SC in 
+    symbols = [Symbol_SC(processes) for Symbol_SC in
         AreaSymbol.__subclasses__() +
         LinearSymbol.__subclasses__() +
         PointSymbol.__subclasses__()
@@ -65,8 +71,8 @@ def run(args):
 
     Logger.log(logging.info, "Creating render instance...")
     map_render = render.MapRender(
-        len(blockmap.heightmap[0]),
-        len(blockmap.heightmap)
+        height,
+        width
     )
 
     if args.debug:
@@ -77,6 +83,6 @@ def run(args):
         for symbol in symbols:
             Logger.log(logging.info, f"Building {symbol.__class__.__name__} symbol...", sub=1)
             symbol.render()
-        
+
         Logger.log(logging.info, "Rendering map...")
         map_render.show()

topomc/render.py
@@ -78,9 +78,12 @@ class MapRender:
             graph.set_size_inches(self.width * 100 / scale * 0.393701, self.height * 100 / scale * 0.393701)
             graph.savefig(self.save_loc)
 
-        for line in axes.lines:
+        for line in axes.get_lines():
             line.set_linewidth(
                 line.get_linewidth() * 2**(4 - np.log2(self.max_len)))
+        for line in [line for line in axes.get_lines() if line.get_marker() != "None"]:
+            line.set_markersize(
+                line.get_markersize() * 2**(4 - np.log2(self.max_len)))
 
         window_size = app.settings["Preview size"]
         graph.set_size_inches(8 * window_size, 8 * window_size)

topomc/symbol.py
@@ -20,27 +20,33 @@ class Symbol:
     def __init__(self, processes, klass):
         return next(proc for proc in processes if isinstance(proc, klass))
 
-    def set_properties(self): 
+    def set_properties(self):
         raise NotImplementedError("Cannot set properties of unspecified symbol type")
-    def plot(self): 
+
+    def plot(self):
         raise NotImplementedError("Cannot plot unspecified symbol type")
+
 
 class AreaSymbol(Symbol):
     def set_properties(self, fillcolor, bordercolor, borderwidth):
         self.fillcolor = fillcolor
         self.bordercolor = bordercolor
         self.borderwidth = borderwidth / 3
-    
+
     def plot(self, area):
-        plt.fill(*Coordinates.to_list(area), facecolor=self.fillcolor, edgecolor=self.bordercolor, linewidth=self.borderwidth)
+        plt.fill(*Coordinates.to_list(area), facecolor=self.fillcolor, edgecolor=self.bordercolor,
+                 linewidth=self.borderwidth)
+
 
 class LinearSymbol(Symbol):
     def set_properties(self, color, linewidth):
         self.color = color
         self.linewidth = linewidth / 3
-    
+
+
     def plot(self, line):
         plt.plot(*Coordinates.to_list(line), color=self.color, linewidth=self.linewidth)
+
 
 class PointSymbol(Symbol):
     def set_properties(self, color, pointsize=1, icon=None):
@@ -48,9 +54,10 @@ class PointSymbol(Symbol):
         if icon:
             self.icon = icon
         else:
-            doc = minidom.parse(path.join(path.dirname(__file__), "assets", "symbols", f"{self.__class__.__name__}.svg"))
+            doc = minidom.parse(
+                path.join(path.dirname(__file__), "assets", "symbols", f"{self.__class__.__name__}.svg"))
             icon = parse_path(
-                [path.getAttribute('d') for path in doc.getElementsByTagName('path')][0]
+                [p.getAttribute('d') for p in doc.getElementsByTagName('path')][0]
             )
             doc.unlink()
 
@@ -59,7 +66,7 @@ class PointSymbol(Symbol):
             icon.vertices -= icon.vertices.mean(axis=0)
             self.icon = icon
 
-        self.pointsize = pointsize * 5
+        self.pointsize = pointsize * 2
 
     def plot(self, point):
-        plt.plot(point.x, point.y, color=self.color, marker=self.icon, markersize=self.pointsize)
+        plt.plot(point.x, point.y, color=self.color, marker=self.icon, markersize=self.pointsize, linewidth=0)